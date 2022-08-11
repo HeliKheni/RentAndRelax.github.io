cae00562fa7d2721ad3e3264672675fa61fb1f71
Heli kheni <helipatel56789@gmail.com>
2022-08-11 10:37:23 -0400

    back

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,20 +1,5 @@
 
 
-// const properties = [
-//     {
-//         feature : "2 room Apartment",
-//         address : "Jane Street",
-//         views : 143,
-//         likes : 4
-//     },
-//     {
-//         feature : "2 room Basement",
-//         address : "Wilson Ave",
-//         views : 100,
-//         likes : 23
-//     },
-// ];
-
 function myFunction1() {
     var x = document.getElementById("myDIV1");
     if (x.style.display === "none") {
